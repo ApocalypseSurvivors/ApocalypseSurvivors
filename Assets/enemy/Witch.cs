0677c9ad96fcbb3d0903b51fd9f526e51583223a
kosumi <chankocyo@gmail.com>
2024-11-03 21:18:35 -0500

    Add animation and state machine

--- Assets/enemy/Witch.cs
+++ Assets/enemy/Witch.cs
@@ -6,8 +6,6 @@ using UltimateXR.Mechanics.Weapons;
 
 public class Witch : MonoBehaviour
 {
-    [SerializeField]
-    int HP = 100;
     private Animator animator;
     private NavMeshAgent navAgent;
     private UxrActor actor;
@@ -17,17 +15,36 @@ public class Witch : MonoBehaviour
         animator = GetComponent<Animator>();
         navAgent = GetComponent<NavMeshAgent>();
         actor = GetComponent<UxrActor>();
-        actor.Life = HP;
+
+        // actor.DamageReceived += HandleDamage;
     }
+
+    void HandleDamage(object sender, UxrDamageEventArgs e) {
+        if (actor.Life <= 0) {
+            int randomValue = Random.Range(0, 2);
+            if (randomValue == 0) {
+                animator.SetTrigger("Die1");
+            } else {
+                animator.SetTrigger("Die2");
+            }
+        }
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (navAgent.velocity.magnitude > 0.1f) {
+        navAgent.enabled = !actor.IsDead;
+        if (navAgent.enabled && navAgent.velocity.magnitude > 0.1f) {
             animator.SetBool("isWalking", true);
         } else {
             animator.SetBool("isWalking", false);
         }
         
+    }
+
+    private void OnDestroy()
+    {
+      actor.DamageReceived -= HandleDamage;
     }
 }
