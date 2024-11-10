afb9e860b6b639653543577c45fb0ba9d48fe6f5
kosumi <chankocyo@gmail.com>
2024-11-10 18:48:55 -0500

    Add FSM; enemy attack state not exit, need fix

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -57,13 +57,13 @@ public class Enemy : MonoBehaviour
                     // Debug.Log("Debug null Player ");
                 }
                 // Debug.Log("Debug set Player ");
-                navAgent.destination = target;
+                // navAgent.destination = target;
 
-                if (navAgent.velocity.magnitude > 0.1f) {
-                    animator.SetBool("isWalking", true);
-                } else {
-                    animator.SetBool("isWalking", false);
-                }
+                // if (navAgent.velocity.magnitude > 0.1f) {
+                //     animator.SetBool("isWalking", true);
+                // } else {
+                //     animator.SetBool("isWalking", false);
+                // }
             if (rb.IsSleeping())
             {
                 ReactivateNavMeshAgent();
---
+++ Assets/ZombieAttacjState.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieAttacjState : StateMachineBehaviour
+{
+    UnityEngine.AI.NavMeshAgent agent;
+    Transform player;
+    [SerializeField] float stopAttackingDistance = 2.5f;
+
+    private void LookAtPlayer()
+    {
+        Vector3 direction = player.position - agent.transform.position;
+        agent.transform.rotation = Quaternion.LookRotation(direction);
+
+        var yRotation = agent.transform.eulerAngles.y;
+        agent.transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       LookAtPlayer();
+       agent.SetDestination(player.position);
+       animator.transform.LookAt(player);  
+       float distanceFromPlayer = Vector3.Distance(player.position, animator.transform.position);
+       if (distanceFromPlayer > stopAttackingDistance) {
+           animator.SetBool("isAttacking", false);
+       }
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
