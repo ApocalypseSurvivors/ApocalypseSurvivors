d11d27a3c6db791480b1e9607dade7f5528b8aec
kosumi <chankocyo@gmail.com>
2024-11-18 00:35:24 -0500

    Update scene

--- Assets/key.cs
+++ Assets/key.cs
@@ -5,9 +5,11 @@ public class DoorDestroyer : MonoBehaviour
 {
     public GameObject door; // Assign the door GameObject in the Inspector
     private UxrGrabbableObject grabbableObject;
+    private GameObject painting; 
 
     void Start()
     {
+        painting = GameObject.FindWithTag("painting");
         grabbableObject = GetComponent<UxrGrabbableObject>();
 
         if (grabbableObject == null)
@@ -24,9 +26,28 @@ public class DoorDestroyer : MonoBehaviour
     void Update()
     {
         // Check if the object is being grabbed
-        if (grabbableObject != null && grabbableObject.IsBeingGrabbed)
+        // if (grabbableObject != null && grabbableObject.IsBeingGrabbed)
+        // {
+        //     Destroy(door);
+        // }
+        Vector3 relativePosition = painting.transform.InverseTransformPoint(transform.position);
+
+        // Check if A is on B's positive X-axis
+        if (relativePosition.x > 1)
         {
             Destroy(door);
+            // Debug.Log("Object A is on the positive X-axis of Object B!");
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        // Debug.Log("Debug Collider!");
+        // Check if the other GameObject has the tag "painting"
+        if (other.gameObject.CompareTag("painting"))
+        {
+            Destroy(door);
+            // Perform your custom logic here
         }
     }
 }
