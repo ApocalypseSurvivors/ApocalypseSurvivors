d840956f08588414646d2a20aa2052fb3b248cc5
kosumi <chankocyo@gmail.com>
2024-12-08 12:16:25 -0500

    Add smart watch

--- Assets/witch_enemy.cs
+++ Assets/witch_enemy.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class witch_enemy : MonoBehaviour
 {
     // Start is called before the first frame update
-    public crypto_enemy_hand crypto_Enemy_Hand;
-    public int crypto_enemy_damage;
+    // public crypto_enemy_hand crypto_Enemy_Hand;
+    // public int crypto_enemy_damage;
     // Start is called before the first frame update
     void Start()
     {
-        crypto_Enemy_Hand.damage = crypto_enemy_damage;
+        // crypto_Enemy_Hand.damage = crypto_enemy_damage;
     }
 
     // Update is called once per frame
